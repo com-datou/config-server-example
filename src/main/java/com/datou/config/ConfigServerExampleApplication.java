cb56f394dadf4ef79b344d472c10b17b3b519393
michael_sun <michael.sun@dyuanzi.com>
2019-10-16 14:45:17 +0800

    change for: add config to config server

--- src/main/java/com/datou/config/ConfigServerExampleApplication.java
+++ src/main/java/com/datou/config/ConfigServerExampleApplication.java
@@ -2,10 +2,12 @@ package com.datou.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableConfigServer
 public class ConfigServerExampleApplication {
 
 	public static void main(String[] args) {
